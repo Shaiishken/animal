527734abf63e0d2200ac897d99270bff61381338
Bekarys Kenzhebek <bekarys_kenzhebek@icloud.com>
2024-12-29 15:29:53 +0500

    Created animal class and its subclasses

---
+++ src/School.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class School {
+    private List<Person> members;
+
+    public School() {
+        this.members = new ArrayList<>();
+    }
+
+    public void addMember(Person member) {
+        members.add(member);
+    }
+
+    public List<Person> getMembers() {
+        return members;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("School Members:\n");
+        for (Person member : members) {
+            result.append(member.toString()).append("\n");
+        }
+        return result.toString();
+    }
+}
+
